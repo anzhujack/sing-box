90335a19cb3fab7c98da55648125a81518916716
root <root@ImmortalWrt.lan>
2026-07-03 05:52:40 +0000

    Revert "fix(clashapi): make dashboard connection hosts unique"
    This reverts commit 858446326581423776d7905e1d514199ef95b33b.

--- experimental/clashapi/connections.go
+++ experimental/clashapi/connections.go
@@ -76,9 +76,6 @@ func (c connectionObject) MarshalJSON() ([]byte, error) {
 	if displayHost != "" && destinationAddr.IsValid() && displayHost != destinationAddr.String() {
 		displayHost = F.ToString(displayHost, "@", destinationAddr)
 	}
-	if displayHost != "" && c.Metadata.Source.Port != 0 {
-		displayHost = F.ToString(displayHost, "#", c.Metadata.Source.Port)
-	}
 	remoteDestination := ""
 	if destinationAddr.IsValid() {
 		remoteDestination = M.SocksaddrFrom(destinationAddr, c.Metadata.Destination.Port).String()
